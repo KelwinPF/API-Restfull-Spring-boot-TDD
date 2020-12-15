d3c1eb2bb6328f5d12b60ccccf365b8738cfcc0c
Kelwin Ferreira <kelwinpferreira@gmail.com>
2020-12-15 16:24:45 -0300

    swagger security

--- src/main/java/com/wallet/security/config/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/wallet/security/config/JwtAuthenticationTokenFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,6 +23,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     private static final String AUTH_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
 
+    @Qualifier("JwtUserDetailsServiceImpl")
     @Autowired
     private UserDetailsService userDetailsService;
 
--- src/main/java/com/wallet/security/config/SecurityConfiguration.java
+++ src/main/java/com/wallet/security/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.wallet.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -27,8 +28,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
 
+    @Qualifier("JwtUserDetailsServiceImpl")
     @Autowired
     private UserDetailsService userDetailsService;
+
+    private static final String[] AUTH_WHITELIST = {
+
+            // -- swagger ui
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
 
     @Autowired
     public void configureAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
@@ -53,6 +64,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll();
         http.csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
                 .antMatchers("/auth/**", "/configuration/security", "/webjars/**", "/user/**", "/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/hello-world")
